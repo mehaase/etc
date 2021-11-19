0aa6a0d4aba7b3b4e04bfa69d861d6e354850852
Mark E. Haase <mehaase@gmail.com>
2021-11-18 21:00:42 -0500

    Add `less` options

--- config.fish
+++ config.fish
@@ -4,5 +4,8 @@ abbr --add --global dexec "docker exec -it (paste) bash"
 abbr --add --global ff "git merge --ff-only origin/(git branch --show-current)"
 abbr --add --global pushup "git push -u origin (git branch --show-current)"
 
+# Set `less` options.
+set -gx LESS NR
+
 # Source the local fish config:
 source "$HOME/.config/fish/config.local.fish"
