a4c879cae4d1886a8bdea36dff036d4bed21fed6
Mark E. Haase <mehaase@gmail.com>
2021-11-26 21:53:22 -0500

    Ensure OMF is installed and choose a theme

--- install.fish
+++ install.fish
@@ -4,6 +4,21 @@
 set SCRIPT_PATH (status --current-filename)
 set REPO_PATH (dirname (realpath "$PWD/$SCRIPT_PATH"))
 
+# Make sure oh-my-fish is installed.
+set omf (which omf)
+if test -z $omf
+    echo "Installing oh-my-fish..."
+    curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+else
+    echo "Existing oh-my-fish found at $omf"
+end
+
+# Set default theme.
+set omf_theme "agnoster"
+echo "Setting oh-my-fish theme to $omf_theme..."
+omf install $omf_theme
+omf theme $omf_theme
+
 # Create config.fish
 set CONFIG_FISH_SOURCE "$REPO_PATH/config.fish"
 set CONFIG_FISH_TARGET "$HOME/.config/fish/config.fish"
